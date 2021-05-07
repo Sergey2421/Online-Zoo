add212ac431837667cc6553c7647215d85fbe772
Sergey Kakadey <sergey_kakadey@mail.ru>
2021-05-07 05:18:38 +0300

    fix: change border color for active logo in Map

--- Online-Zoo/js/map.js
+++ Online-Zoo/js/map.js
@@ -1,28 +1,32 @@
 const markers = document.querySelectorAll('.markers');
 const boxes = document.querySelectorAll('.box');
 const main = document.querySelector('.main');
+const btns = document.querySelectorAll('.nav_btns');
 
 function ResetMark(elem) {
     if (elem.contains(main) || elem === main) {
         markers.forEach((elem) => elem.classList.remove('active'));
         boxes.forEach((e) => e.classList.add('invisible'));
+        btns.forEach((elem) => elem.classList.remove('active_logo'));
     }
 }
 
 function ChangeMark(elem) {
     markers.forEach((elem) => elem.classList.remove('active'));
+    btns.forEach((elem) => elem.classList.remove('active_logo'));
+    boxes.forEach((e) => e.classList.add('invisible'));
     elem.classList.add('active');
     if (elem.parentElement.classList.contains('mark-eagle')) {
-        boxes.forEach((e) => e.classList.add('invisible'));
+        btns[2].classList.add('active_logo');
         boxes[0].classList.remove('invisible');
     } else if (elem.parentElement.classList.contains('mark-gorilla')) {
-        boxes.forEach((e) => e.classList.add('invisible'));
+        btns[3].classList.add('active_logo');
         boxes[1].classList.remove('invisible');
     } else if (elem.parentElement.classList.contains('mark-panda')) {
-        boxes.forEach((e) => e.classList.add('invisible'));
+        btns[0].classList.add('active_logo');
         boxes[2].classList.remove('invisible');
     } else {
-        boxes.forEach((e) => e.classList.add('invisible'));
+        btns[1].classList.add('active_logo');
         boxes[3].classList.remove('invisible');
     }
 }
